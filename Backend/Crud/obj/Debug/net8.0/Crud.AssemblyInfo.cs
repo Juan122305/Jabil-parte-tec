194d121dfe1b33503dd4f377c056a7ad593354b4
Juan122305 <A01743268@tec.mx>
2026-09-10 13:33:45 -0600

    Aplicacion completa

--- Backend/Crud/obj/Debug/net8.0/Crud.AssemblyInfo.cs
+++ Backend/Crud/obj/Debug/net8.0/Crud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Crud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a20813265d81d2b0a66aa70e1ef21d91fab87a0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c82327411f876230f39124e67a7c95d59d03f39")]
 [assembly: System.Reflection.AssemblyProductAttribute("Crud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Crud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
